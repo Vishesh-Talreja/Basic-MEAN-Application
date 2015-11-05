6e512bdaa513d464f349d4b6d812ac5b03cc903f
Vishesh <visheshtalreja@yahoo.com>
2015-11-04 23:08:48 -0500

    starting angular mvc

--- nodeExpress/app.js
+++ nodeExpress/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! Starting work on angular');
 });
 
 var server = app.listen(3000, function () {
